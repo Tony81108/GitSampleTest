c1bc2033cbf9115d783bc3e52077fdd747bd3b89
Tony Lu <tony81108@gmail.com>
2022-11-05 14:51:51 +0800

    Add Math Branch

--- GitSample/src/testApp/MainApp.java
+++ GitSample/src/testApp/MainApp.java
@@ -5,6 +5,16 @@ public class MainApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("2nd Version");
+		//組長新增的
+		Math cMath = new Math();
+		System.out.println();
+		System.out.println(cMath.add(5, 5));
 	}
 
+}
+class Math{
+	int add(int x, int y) {
+		return x+y;
+	}
+	
 }
